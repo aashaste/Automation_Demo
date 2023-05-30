8b6bd84b50e70cdc7b2eb67cf8896c0092ecc421
Swapnil A <swapnil.agalave@gmail.com>
2023-05-30 11:13:22 +0530

    First Comit

--- src/test/java/com/automation/Demo.java
+++ src/test/java/com/automation/Demo.java
@@ -1,11 +1,41 @@
 package com.automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Demo {
+	public static WebDriver driver=null;
+  @Test
+  public void TC_1() {
+	  
+	  WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+  	
+      String baseUrl = "http://demo.guru99.com/test/newtours/";
+      String expectedTitle = "Welcome: Mercury Tours";
+      String actualTitle = "";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test maven project");
+      // launch Fire fox and direct it to the Base URL
+      driver.get(baseUrl);
 
-	}
+      // get the actual value of the title
+      actualTitle = driver.getTitle();
 
-}
+      /*
+       * compare the actual title of the page with the expected one and print
+       * the result as "Passed" or "Failed"
+       */
+      if (actualTitle.contentEquals(expectedTitle)){
+          System.out.println("Test Passed!");
+      } else {
+          System.out.println("Test Failed");
+      }
+     
+      
+  }
+
+  }
+
